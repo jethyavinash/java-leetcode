bfe4be5d0e5168b30a5f8d607a204ef1361f71d9
avinash <avinashjethy89@gmail.com>
2024-11-24 12:46:36 +0530

    leet code

---
+++ src/com/practice/coding/graph/GraphDFS11162024.java
@@ -0,0 +1,72 @@
+package com.practice.coding.graph;
+
+import java.util.*;
+
+public class GraphDFS11162024 {
+    private List<List<Integer>> adjList;
+    private int vertices;
+
+    public GraphDFS11162024(int vertices){
+        this.vertices = vertices;
+        this.adjList = new ArrayList<>();
+        for(int i=0;i<vertices;i++){
+            adjList.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int u,int v){
+        adjList.get(u).add(v);
+    }
+
+    public void DFS(int start){
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[vertices];
+        stack.push(start);
+        visited[start] = true;
+        while (!stack.isEmpty()){
+            int curr = stack.pop();
+            System.out.print(curr+" ");
+            for(int neighbour : adjList.get(curr)){
+                if(!visited[neighbour]){
+                    visited[neighbour] = true;
+                    stack.push(neighbour);
+                }
+            }
+        }
+    }
+
+    public void BFS(int start){
+        System.out.println();
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[vertices];
+        queue.add(start);
+        visited[start] = true;
+        while (!queue.isEmpty()){
+            int curr = queue.poll();
+            System.out.print(curr+" ");
+            for(Integer neighbour : adjList.get(curr)){
+                if(!visited[neighbour]){
+                    visited[neighbour] = true;
+                    queue.add(neighbour);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        GraphDFS11162024 g = new GraphDFS11162024(7);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 5);
+        g.addEdge(2, 6);
+
+        g.DFS(0);
+        g.BFS(0);
+
+    }
+
+
+}
---
+++ src/com/practice/coding/graph/GraphIfPathPresent.java
@@ -0,0 +1,49 @@
+package com.practice.coding.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphIfPathPresent {
+    public static void main(String[] args) {
+        int vertices = 4;
+        int[][] edges = new int[][]{{0,1},{0,2},{1,0},{1,3},{2,0},{2,3},{3,1},{3,2}};
+        int start = 0;
+        int end = 3;
+        boolean result = isPathExist(vertices,edges,start,end);
+        System.out.println(result);
+    }
+
+    private static boolean isPathExist(int vertices, int[][] edges, int start, int end) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i =0;i<vertices;i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] edge: edges){
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[vertices];
+        return dfs(graph,start,end,visited);
+
+
+    }
+
+    public static boolean dfs(List<List<Integer>> graph, int start, int end, boolean[] visited) {
+        System.out.println("111");
+        if(start == end){
+            System.out.println("222");
+            return true;
+        }
+        visited[start] = true;
+        for(int neighbour : graph.get(start)){
+            if(!visited[neighbour]){
+                if(GraphIfPathPresent.dfs(graph,neighbour,end,visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
